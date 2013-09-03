6d72970646689265a35f20899a5b1a61f1d28dc4
Marco Lau <lau.career@gmail.com>
2013-09-03 20:14:14 +0800

    Add random_number_generator()

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 
 using namespace std;
+
+/* Generate a random number seeded with time value for better randomness */
+int random_number_generator(int lowest, int highest)
+{
+    srand((unsigned)time(0));
+    int random_integer;
+    int range = highest-lowest;
+    random_integer = lowest+(rand()%(range+1));
+    cout << random_integer << endl;
+}
 
 int main()
 {
@@ -12,6 +24,12 @@ int main()
         cout << "Enter value of argv[" << i+1 << "]:" << endl;
         cin >> argv[i];
     }
+
+    int lowest = argv[0];
+    int highest = argv[1];
+    int divisor = argv[2];
+
+    random_number_generator(lowest, highest);
 
     return 0;
 }
