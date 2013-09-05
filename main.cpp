a49dd052c1f2a9d8c34a3bd70f4015d3f2c163f4
Marco Lau <lau.career@gmail.com>
2013-09-05 13:10:06 +0800

    Change input entry from using arrays to using argv

--- main.cpp
+++ main.cpp
@@ -14,23 +14,28 @@ int random_number_generator(int lowest, int highest)
     return random_integer;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    int argv[3];
-    int argvLen = sizeof(argv)/sizeof(*argv);
+    //initscr();
+    //int argv[3];
+    //int argvLen = sizeof(argv)/sizeof(*argv);
 
-    refresh();
-
+    int lowest = atoi(argv[1]);
+    int highest = atoi(argv[2]);
+    int divisor = atoi(argv[3]);
+/*
     for (int i=0; i<argvLen; i++)
     {
+        printw("Enter value of argv[%d]: ", i+1);
+        int n = getch();
+        cout << endl;
+        argv[i] = n;
+        refresh();
+
         cout << "Enter value of argv[" << i+1 << "]:" << endl;
         cin >> argv[i];
     }
-
-    int lowest = argv[0];
-    int highest = argv[1];
-    int divisor = argv[2];
-
+*/
     int random_number;
     srand((unsigned)time(0));
 
@@ -41,5 +46,6 @@ int main()
         sleep(1);
     } while(random_number % divisor != 0 );
 
+    //endwin();
     return 0;
 }
